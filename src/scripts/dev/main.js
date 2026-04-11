ac0a9e2f6d5116ae230f12389bb042c128a43370
ystepunina <FlexSEP@gmail.com>
2026-04-11 12:48:33 +0700

    page-header.html

--- src/scripts/dev/main.js
+++ src/scripts/dev/main.js
@@ -1,5 +1,46 @@
 (function () {
   "use strict";
 
-  //ваш код
+   const root = document.documentElement;
+   const navToggle = document.querySelector("#js-navToggle");
+
+   navToggle.addEventListener("click",function() {
+      root.classList.toggle("");
+   });
 })();
+
+const eventPP = document.querySelector("#js-eventPP");
+
+  if (eventPP) {
+    const eventOpenBtn = document.querySelector("#js-eventOpenBtn");
+
+    const closeEventPP = function (event) {
+      function close() {
+        document.removeEventListener("keyup", closeEventPP);
+        eventPP.removeEventListener("click", closeEventPP);
+
+        root.classList.remove("show-event-popup");
+      }
+
+      switch (event.type) {
+        case "keyup":
+          if (event.key === "Escape") close();
+          break;
+        case "click":
+          if (
+            event.target === this ||
+            event.target.classList.contains("js-ppCloseBtn")
+          )
+            close();
+          break;
+      }
+    };
+
+    eventOpenBtn.addEventListener("click", function () {
+      root.classList.add("show-event-popup");
+
+      document.addEventListener("keyup", closeEventPP);
+      eventPP.addEventListener("click", closeEventPP);
+    });
+  }
+
