43c652b083de7476bebfb273f95916cda66b8f30
ystepunina <FlexSEP@gmail.com>
2026-04-11 16:11:16 +0700

    main.js

--- src/scripts/dev/main.js
+++ src/scripts/dev/main.js
@@ -1,3 +1,5 @@
+const { root } = require("postcss");
+
 (function () {
   "use strict";
 
@@ -7,40 +9,32 @@
    navToggle.addEventListener("click",function() {
       root.classList.toggle("");
    });
-})();
+
 
 const eventPP = document.querySelector("#js-eventPP");
 
   if (eventPP) {
+
+
     const eventOpenBtn = document.querySelector("#js-eventOpenBtn");
 
-    const closeEventPP = function (event) {
-      function close() {
-        document.removeEventListener("keyup", closeEventPP);
-        eventPP.removeEventListener("click", closeEventPP);
-
-        root.classList.remove("show-event-popup");
-      }
-
-      switch (event.type) {
-        case "keyup":
-          if (event.key === "Escape") close();
-          break;
-        case "click":
-          if (
-            event.target === this ||
-            event.target.classList.contains("js-ppCloseBtn")
-          )
-            close();
-          break;
-      }
-    };
-
-    eventOpenBtn.addEventListener("click", function () {
-      root.classList.add("show-event-popup");
-
-      document.addEventListener("keyup", closeEventPP);
-      eventPP.addEventListener("click", closeEventPP);
+    eventOpenBtn.addEventListener("click",function() {
+       root.classList.add("show-event-popup");
     });
+
+    eventPP.addEventListener("click" ,function(event) {
+      if (event.target === this || event.target.classList.contains("js-ppCloseBtn")) {
+          root.classList.remove("show-event-popup");
+      }
+    });
+
+    document.addEventListener("keyup", function(event) {
+      if (event.key === "Escape") {
+          root.classList.remove("show-event-popup");
+      }
+    });
+
   }
 
+  })();
+  
